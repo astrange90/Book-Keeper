ea4e6c804d0f216b3259587ee3b1c5b76160030c
astrange90 <astrange90@hotmail.com>
2024-04-14 17:35:58 -0700

    added schemas, slight adjust to front end, and updated README

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,38 @@
+const typeDefs = `
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [Book]
+  }
+
+  type Book {
+    authors: [String]
+    description: String!
+    title: String!
+    bookId: String!
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    createUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    # removeUser: User
+  
+    saveBook(authors: [String], description: String!, title: String!, bookId: String!, image: String, link: String ): User
+    removeBook(bookId: String!): User
+  }
+`;
+
+module.exports = typeDefs;
